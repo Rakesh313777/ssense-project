d596ff81d107e67256bc01ff7162129236327cdd
neha singh <nehasingh12345310@gmail.com>
2022-05-08 23:19:40 +0530

    made the signup

---
+++ signup/sign.js
@@ -0,0 +1,16 @@
+var form = document.querySelector("form")
+
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+
+    var data = {
+        username:form.username.value,
+        email:form.email.value,
+        password:form.password.value,
+        enter:form.enter.value,
+    }
+    localStorage.setItem("userData",JSON.stringify
+    (data));
+    window.location.href="login.html"
+})
+
